365c8bb71f7a5bc4b7335673b0307a5463f4b2a9
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-16 16:35:48 +0300

    Some Nuget Packages Added

---
+++ Swagger/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Swagger.Models
+{
+    public  class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime? Date { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ Swagger/Models/SwaggerDbContext.cs
@@ -0,0 +1,49 @@
+﻿ using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Swagger.Models
+{
+    public partial class SwaggerDbContext : DbContext
+    {
+        public SwaggerDbContext()
+        {
+        }
+
+        public SwaggerDbContext(DbContextOptions<SwaggerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+   
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Turkish_CI_AS");
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("product");
+
+                entity.Property(e => e.Category).HasMaxLength(50);
+
+                entity.Property(e => e.Date).HasColumnType("datetime");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
